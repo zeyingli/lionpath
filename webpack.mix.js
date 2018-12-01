c7169106ad026accce9dec5846a97747bea8fa6f
Zeying Li <admin@zeyingli.com>
2018-12-01 16:34:24 -0500

    precompiled assets with webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,34 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .styles([
+         'resources/css/landing/swiper.min.css',
+         'resources/css/landing/bootstrap.min.css',
+         'resources/css/landing/style.css',
+         'resources/css/landing/main.css',
+      ], 'public/css/landing.css')
+   .styles([
+         'resources/css/frontend/bootstrap.min.css',
+         'resources/css/frontend/material-icons.css',
+         'resources/css/frontend/swiper.min.css',
+         'resources/css/frontend/style.css',
+      ], 'public/css/webapp.css')
+   .scripts([
+         'resources/js/landing/jquery-3.2.1.min.js',
+         'resources/js/landing/popper.min.js',
+         'resources/js/landing/bootstrap.min.js',
+         'resources/js/landing/swiper.min.js',
+         'resources/js/landing/main.js',
+      ], 'public/js/landing.js')
+   .scripts([
+         'resources/js/frontend/jquery-3.2.1.min.js',
+         'resources/js/frontend/popper.min.js',
+         'resources/js/frontend/bootstrap.min.js',
+         'resources/js/frontend/jquery.cookie.js',
+         'resources/js/frontend/jquery.sparkline.min.js',
+         'resources/js/frontend/circle-progress.min.js',
+         'resources/js/frontend/swiper.min.js',
+         'resources/js/frontend/main.js',
+      ], 'public/js/webapp.js')
+   .version();
